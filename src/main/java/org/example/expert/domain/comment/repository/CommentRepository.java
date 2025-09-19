6d6a03895270cf61822e514cee630b5100ec38ae
ksg <kasaga234@gmail.com>
2025-09-19 10:26:24 +0900

    2.2 단계

--- src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
+++ src/main/java/org/example/expert/domain/comment/repository/CommentRepository.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    @Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    //@Query("SELECT c FROM Comment c JOIN c.user WHERE c.todo.id = :todoId")
+    // 2.2 단계 코드
+    @Query("SELECT c FROM Comment c JOIN FETCH c.user WHERE c.todo.id = :todoId")
     List<Comment> findByTodoIdWithUser(@Param("todoId") Long todoId);
 }
